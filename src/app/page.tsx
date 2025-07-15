385ac6e8154288a97b0e8dc88f1cce81729e8b5a
hendraaagil <hendraaagil@gmail.com>
2025-07-15 23:03:09 +0700

    style: add background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Quiz from '@/components/quiz'
 
 export default function Home() {
-  return <Quiz />
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
+      <Quiz />
+    </div>
+  )
 }
